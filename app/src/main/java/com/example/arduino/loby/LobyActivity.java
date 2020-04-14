4959fb71e286379b8e6b3dab1331fa4f5d021388
Amit <amither@campus.technion.ac.il>
2020-04-14 09:49:30 +0300

    1. change some screen

--- app/src/main/java/com/example/arduino/loby/LobyActivity.java
+++ app/src/main/java/com/example/arduino/loby/LobyActivity.java
@@ -2,17 +2,24 @@ package com.example.arduino.loby;
 
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.arduino.R;
+import com.example.arduino.gameScreen.GameScreenActivity;
+import com.google.android.gms.common.SignInButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class LobyActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
+    private Button sendDt;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,7 +28,18 @@ public class LobyActivity extends AppCompatActivity {
         info.put("User-Id", 1);
         info.put("NumberShot", 100);
         info.put("NumberOfWins" , 10);
+        sendDt = (Button) findViewById(R.id.LobyButton);
         DatabaseReference dbReff = FirebaseDatabase.getInstance().getReference().child("Users");
-        dbReff.setValue(info);
+        sendDt.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public  void onClick(View v){
+                changeScreen(GameScreenActivity.class);
+            }
+        });
+
+    }
+    public void changeScreen(Class screen){
+        Intent intent = new Intent(this, screen);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/arduino/stats/StatitacsActivity.java
+++ app/src/main/java/com/example/arduino/stats/StatitacsActivity.java
@@ -2,14 +2,33 @@ package com.example.arduino.stats;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import com.example.arduino.R;
+import com.example.arduino.gameScreen.GameScreenActivity;
+import com.example.arduino.menu.MenuActivity;
 
 public class StatitacsActivity extends AppCompatActivity {
-
+    private Button sendDt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_statitacs);
+        sendDt= (Button) findViewById(R.id.StatsMenu);
+        sendDt.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public  void onClick(View v){
+                changeScreen(MenuActivity.class);
+            }
+        });
+
+    }
+    public void changeScreen(Class screen){
+        Intent intent = new Intent(this, screen);
+        startActivity(intent);
     }
 }
+
