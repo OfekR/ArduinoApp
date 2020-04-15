888ff15200126be857a0c5455265742345ea0fb5
Amit <amither@campus.technion.ac.il>
2020-04-15 23:48:04 +0300

    1.New Loby style

--- app/src/main/java/com/example/arduino/loby/LobyActivity.java
+++ app/src/main/java/com/example/arduino/loby/LobyActivity.java
@@ -3,43 +3,84 @@ package com.example.arduino.loby;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.arduino.R;
 import com.example.arduino.gameScreen.GameScreenActivity;
+import com.example.arduino.utilities.MediaPlayerWrapper;
 import com.google.android.gms.common.SignInButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class LobyActivity extends AppCompatActivity {
-    private FirebaseAuth mAuth;
-    private Button sendDt;
+    private MediaPlayer mMediaPlayer;
+    int mCurrentVideoPosition;
+    private VideoView videoBG;
+    private MediaPlayerWrapper mySong;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loby);
-        Map<String ,Integer> info = new HashMap<String, Integer>();
-        info.put("User-Id", 1);
-        info.put("NumberShot", 100);
-        info.put("NumberOfWins" , 10);
-        sendDt = (Button) findViewById(R.id.LobyButton);
-        DatabaseReference dbReff = FirebaseDatabase.getInstance().getReference().child("Users");
-        sendDt.setOnClickListener(new View.OnClickListener(){
+        videoBG = (VideoView) findViewById(R.id.videoView);
+        String path = "android.resource://"+getPackageName()+"/"+R.raw.hd1384;
+        Uri uri = Uri.parse(path);
+        videoBG.setVideoURI(uri);
+        videoBG.start();
+        videoBG.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
             @Override
-            public  void onClick(View v){
-                changeScreen(GameScreenActivity.class);
+            public void onPrepared(MediaPlayer mp) {
+                mMediaPlayer =mp;
+                mMediaPlayer.setLooping(true);
+                if(mCurrentVideoPosition !=0){
+                    mMediaPlayer.seekTo(mCurrentVideoPosition);
+                    mMediaPlayer.start();
+                }
             }
         });
 
+
     }
-    public void changeScreen(Class screen){
-        Intent intent = new Intent(this, screen);
-        startActivity(intent);
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mySong.Pause();
+        mCurrentVideoPosition = mMediaPlayer.getCurrentPosition();
+        videoBG.pause();
     }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mySong.Destroy();
+        mMediaPlayer.release();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if(mySong == null) {
+            mySong = new MediaPlayerWrapper(R.raw.songwar,getApplicationContext());
+        }
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mySong.StartOrResume();
+        videoBG.start();
+    }
+
+
 }
